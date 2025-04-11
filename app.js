4d2f944d7faf0615b6f05ed5254c161ac927e4bf
mfadlieputrap <mfadlieputrap@gmail.com>
2025-04-12 01:26:36 +0700

    chore(app): setup express config and dependencies

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import routes from "./routes/index.js";
+import logger from "./utils/logger.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('combine', {stream: logger.stream}));
+app.use('/api', routes);
+
+app.listen(PORT, ()=>{
+	console.log(`Server is running on port: ${PORT}`);
+})
