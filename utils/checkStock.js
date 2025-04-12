8eb43a8ac890631bfd17e432f9d776d36412b150
mfadlieputrap <mfadlieputrap@gmail.com>
2025-04-13 01:11:46 +0700

    feat(utils): add checkStock utility to check if stock available or not

---
+++ utils/checkStock.js
@@ -0,0 +1,16 @@
+import Product from "../models/Product.js";
+
+const checkStock = async (productId, variant, quantity) => {
+  const product = await Product.findById(productId).lean();
+	if (!product) throw new Error("Product not found");
+	
+	if(product.variants.length === 0){
+		if(quantity > product.defaultStock) throw new Error("Not enough stock");
+	}else{
+		const selectedVariant = product.variants.find(v=> v.color === variant.color && v.size === variant.size);
+		if(!selectedVariant) throw new Error("Variant not found");
+		if(quantity > selectedVariant.stock) throw new Error("Not enough stock");
+	}
+}
+
+export default checkStock;
