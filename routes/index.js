747b261617518a8d23e97d6f7f759eaf2dd6678a
mfadlieputrap <mfadlieputrap@gmail.com>
2025-04-14 12:09:43 +0700

    refactor(routes): add cart route to index

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ import userRoute from "./userRoute.js";
 import authRoute from "./authRoute.js";
 import addressRoute from "./addressRoute.js";
 import bankAccountRoute from "./bankAccountRoute.js";
+import cartRoute from "./cartRoute.js";
 const router = express.Router();
 
 router.use('/user', userRoute);
 router.use('/auth', authRoute);
 router.use('/address', addressRoute);
 router.use('/bank-account', bankAccountRoute);
+router.use('/cart', cartRoute);
 
 export default router;
