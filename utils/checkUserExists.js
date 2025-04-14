eb51da3dd2e6cbb288c4e7b1666515bd69c117a2
mfadlieputrap <mfadlieputrap@gmail.com>
2025-04-14 20:44:14 +0700

    refactor(utils): Update checkUserExists to validate whether userId is valid mongoId

--- utils/checkUserExists.js
+++ utils/checkUserExists.js
@@ -1,13 +1,25 @@
 import User from '../models/User.js';
+import mongoose from "mongoose";
 
-const checkUserExists = async (userId) => {
-	const user = await User.findById(userId).select("-password");
-	if(!user){
-		const error = new Error('User not found');
-		error.status = 404;
-		throw error;
+const checkUserExists = async (req, res, next) => {
+	try {
+		const userId = req.user.id;
+		
+		if(!userId || !mongoose.Types.ObjectId.isValid(userId)){
+			return res.status(401).json({ message: 'Invalid or missing user ID' });
+		}
+		
+		const user = await User.findById(userId).select("-password");
+		if (!user) {
+			return res.status(404).json({ message: 'User not found' });
+		}
+		
+		req.userData = user;
+		
+		next();
+	} catch (e) {
+		return res.status(500).json({ error: e.message });
 	}
-	return user;
 };
 
 export default checkUserExists;
