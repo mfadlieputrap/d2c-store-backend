2a57838af9371351a714dc7fd3924047530dd9f3
mfadlieputrap <mfadlieputrap@gmail.com>
2025-04-19 03:49:49 +0700

    chore(routes): register product and wishlist routes in index.js

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,13 @@ import addressRoute from "./addressRoute.js";
 import bankAccountRoute from "./bankAccountRoute.js";
 import cartRoute from "./cartRoute.js";
 import {allowRoles, authJwt} from "../middleware/authMiddleware.js";
+import productRoute from "./productRoute.js";
+import wishlistRoute from "./wishlistRoute.js";
 const router = express.Router();
 
 router.use('/user', userRoute);
+router.use('/products', productRoute);
+router.use('/wishlists', wishlistRoute);
 router.use('/auth', authRoute);
 router.use('/address', addressRoute);
 router.use('/bank-account', bankAccountRoute);
