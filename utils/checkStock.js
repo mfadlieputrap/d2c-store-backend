27ed5de2f06f611ce075119af71d8b25e1c0ecec
mfadlieputrap <mfadlieputrap@gmail.com>
2025-04-26 14:20:11 +0700

    refactor(utils): update checkStock helper operations

--- utils/checkStock.js
+++ utils/checkStock.js
@@ -7,7 +7,8 @@ const checkStock = async (productId, variant, quantity) => {
 	if(product.variants.length === 0){
 		if(quantity > product.defaultStock) throw new Error("Not enough stock");
 	}else{
-		const selectedVariant = product.variants.find(v=> v.color === variant.color && v.size === variant.size);
+		const availableVariant = product.variants.find(v=> v.color === variant.color);
+		const selectedVariant = availableVariant.sizes.find(s => s.size === variant.size);
 		if(!selectedVariant) throw new Error("Variant not found");
 		if(quantity > selectedVariant.stock) throw new Error("Not enough stock");
 	}
