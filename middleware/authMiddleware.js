00a07d7f6c184142c65792ed2cce16e9168df4b2
mfadlieputrap <mfadlieputrap@gmail.com>
2025-04-26 14:37:15 +0700

    chore(middlewares): add console.error to authJwt in authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,7 +3,6 @@ import checkUserExists from "../utils/checkUserExists.js";
 
 export const authJwt = async (req, res, next) => {
 	try{
-		console.log("Middleware: Checking token...");
 		const authHeader= req.headers.authorization;
 		
 		if( !authHeader || !authHeader.startsWith('Bearer ') ){
@@ -11,21 +10,22 @@ export const authJwt = async (req, res, next) => {
 		}
 		
 		const token = authHeader.split(" ")[1];
-		
 		req.user = await jwt.verify(token, process.env.JWT_SECRET);
-		console.log("Decoded user from token: ", req.user);
 		
 		next();
 	}catch(e){
+		console.error('Verify Token Error: ', e.message);
 		return res.status(500).json({ error: e.message });
 	}
 }
 
 export const allowRoles = (role) => {
 	return (req, res, next) => {
-		if(req.user.role !== role){
+		console.log("Checking roles");
+		if(!role.includes(req.user.role)){
 			return res.status(403).json({ message: "Forbidden: Access denied!" })
 		}
+		
 		next();
 	}
 }
