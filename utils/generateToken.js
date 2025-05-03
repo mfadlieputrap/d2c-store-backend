467b470915caf201d7d36d1ae6cbb3a8bec1ca70
mfadlieputrap <mfadlieputrap@gmail.com>
2025-05-03 20:38:11 +0700

    chore(utils): delete console.log

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (user) => {
 	return jwt.sign({
-		userId: user.id,
+		id: user.id,
 		role: user.role
 	}, process.env.JWT_SECRET, { expiresIn: "1d"});
 }
